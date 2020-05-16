9abe1cbc7c4680c66532b084b78138323dcddffe
lchuang8211 <64480625+lchuang8211@users.noreply.github.com>
2020-05-16 18:17:40 +0800

    no message

---
+++ URLconnect.php
@@ -0,0 +1,16 @@
+<?php
+	$input = file_get_contents("php://input",'r');
+	
+
+	if($input!=null && $input!=""){ 
+        // str_replace("\n", "", $input1)
+        if(substr($input,0,3) == pack("CCC",0xEF,0xBB,0xBF)) 
+            $input = substr($input,3);
+        $input = json_decode( trim($input,chr(239).chr(187).chr(191)),  true);
+        // echo var_dump($input)."+ ";
+        $input = $input["phoneDataJson"];
+        // echo "json_last_error : ".json_last_error()." - ";
+    } 
+    echo "connect ok ";
+    echo $input;
+?>
